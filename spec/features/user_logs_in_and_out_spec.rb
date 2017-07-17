16396198d4b4b10a7461ab1b10b468c6a3383006
Rachel Warbelow <rachelwarbelow@gmail.com>
2017-07-17 17:26:30 -0600

    change from hash to object

--- spec/features/user_logs_in_and_out_spec.rb
+++ spec/features/user_logs_in_and_out_spec.rb
@@ -10,19 +10,19 @@ RSpec.feature "User logs in and out" do
     user = User.create(user_attributes)
 
     visit login_path
-    fill_in "Username", with: user_attributes[:username]
+    fill_in "Username", with: user.username
     fill_in "session[password]", with: user_attributes[:password]
 
     click_on "Log In"
 
     expect(current_path).to eq(user_path(user))
-    expect(page).to have_content("Welcome, #{user_attributes[:username]}")
+    expect(page).to have_content("Welcome, #{user.username}")
     expect(page).to have_content("Successful login")
 
     click_on "Log Out"
 
     expect(current_path).to eq(root_path)
-    expect(page).to_not have_content("#{user_attributes[:username]}")
+    expect(page).to_not have_content("#{user.username}")
     expect(page).to have_content("Goodbye")
   end
 end
