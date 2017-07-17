9755997e7b0dbbfa2e22eab4e05ba2fb562f0d4f
Rachel Warbelow <rachelwarbelow@gmail.com>
2017-07-17 17:55:47 -0600

    implemented

--- spec/features/user_logs_in_and_out_spec.rb
+++ spec/features/user_logs_in_and_out_spec.rb
@@ -10,16 +10,17 @@ RSpec.feature "User logs in and out" do
     user = User.create(user_attributes)
 
     visit login_path
+
     fill_in "Username", with: user.username
     fill_in "session[password]", with: user_attributes[:password]
 
-    click_on "Log In"
+    click_on "Login"
 
     expect(current_path).to eq(user_path(user))
     expect(page).to have_content("Welcome, #{user.username}")
     expect(page).to have_content("Successful login")
 
-    click_on "Log Out"
+    click_on "Logout"
 
     expect(current_path).to eq(root_path)
     expect(page).to_not have_content("#{user.username}")
