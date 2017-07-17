50356dc7e5a345db25ed873728a8b79f88afb7fb
Rachel Warbelow <rachelwarbelow@gmail.com>
2017-07-17 17:47:46 -0600

    pause

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_attributes)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Successful login"
+      redirect_to @user
+    else
+    end
+  end
+
+  def show
+
   end
 
 private
